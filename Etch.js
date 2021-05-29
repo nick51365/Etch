a09fd47ce728629c4302719fceb4688a8fb8cc34
nick <nick51365@gmail.com>
2021-05-29 13:04:37 -0400

    Added grid to draw

---
+++ Etch.js
@@ -0,0 +1,39 @@
+let gridSize = 16;
+
+let container = document.getElementById("container");
+container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+
+function addBlocks(gridSize){
+    for (i = 0; i < gridSize*gridSize; i++){
+        let block = document.createElement("div");
+        block.setAttribute("class","block");
+        block.style.height = "100%";
+        block.style.width = "100%";
+        block.style.backgroundColor = "white";
+        block.addEventListener("mouseenter",() => block.style.backgroundColor = "purple");
+        container.appendChild(block);
+    }
+};
+document.onload = addBlocks(gridSize);
+
+const resetButton = document.getElementById("resetButton");
+resetButton.addEventListener("click", () => resetColor());
+resetButton.addEventListener("click", () => changeSize());
+
+function resetColor(){
+    const blocks = document.querySelectorAll("div.block");
+    blocks.forEach(block => block.style.backgroundColor = "white");
+}
+
+function changeSize(){
+    removeBlocks(container);
+    gridSize = prompt("New Size (1-100)");
+    addBlocks(gridSize);
+}
+
+function removeBlocks(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
